a8d7333061e760f07fa883ce933d6ec78fe8b4fe
DUCPS <isaacbrocklesby@Isaacs-MacBook-Air.local>
2021-07-29 22:54:39 +0100

    Adding more pages

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
 import React from 'react';
-import cursed_welcome from './art_images/cursed_welcome.jpg'
+import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useLocation
+  } from "react-router-dom";
+
+import Home from "./pages/home.js"
+import Contact from "./pages/contact.js"
+
 
 class App extends React.Component {
     render() {
-      return (
-          <div class="flex items-center" className="welcome-page">
-              <div class="md:flex md:items-center text-center">
-                <div class="md:flex md:pl-2" className="welcome-greeting">
-                    <p class="pl-4 text-3xl font-mono text-blue-gray md:pb-8">
-                        Greetings traveller...
-                    </p>
-                    <div>
-                        <button class="text-7xl font-mono bg-white hover:text-black text-gray-500">
-                            art
-                        </button>
-                    </div>
-                    <div>
-                        <button class="text-7xl font-mono bg-white hover:text-black text-gray-500">
-                            contact
-                        </button>
-                    </div>
-                </div>
-                <img class="md:object-right mx-auto h-auto" src={cursed_welcome} alt="cursed_welcome" />
-              </div>
-          </div>
-      );
-    }
-  }
+        return (
+            <Router>
+                <ModalSwitch />
+            </Router>
+        );
+    };
+}
+
+function ModalSwitch() {
+    let location = useLocation();
+    let background = location.state && location.state.background;
+    return(
+        <Switch location={background || location}>
+            <Route exact path="/" children={<Home /> }/>
+            <Route path="/art" children={<Art />} />
+            <Route path="/contact" children={<Contact />} />
+        </Switch>
+    );
+}
+  
+function Art() {
+    return <h2>Users</h2>;
+}
 
 export default App;
